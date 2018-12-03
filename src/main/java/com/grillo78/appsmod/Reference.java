cef06a9b05c5d13bf31571d7747f253a19ab19e9
grillo78 <sr.grillo79@gmail.com>
2018-12-03 21:46:26 +0100

    Add files via upload

--- src/main/java/com/grillo78/appsmod/Reference.java
+++ src/main/java/com/grillo78/appsmod/Reference.java
@@ -3,7 +3,7 @@ package com.grillo78.appsmod;
 public class Reference {
     public static final String MODID = "appsmod";
     public static final String NAME = "Apps Mod";
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.4";
     public static final String CLIENT_PROXY_CLASS = "";
     public static final String COMMON_PROXY_CLASS = "";
     public static final String WORKING_MC_VERSION = "[1.12.2]";
